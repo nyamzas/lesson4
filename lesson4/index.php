79f7a2bd720c1a7be23ace77d1798e39d9ce75c2
Alexey <captainjackbit@gmail.com>
2019-02-03 14:05:46 +0300

    Выполнил домашнее задание и добавил картинок, чтобы было удобно проверять

---
+++ lesson4/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lesson4</title>
+</head>
+<body>
+    <?php 
+        $file = scandir("img");
+        foreach($file as $value){
+            echo   '<a href="img/' . $value .'"><img src="img/' . $value .'" alt="" width= "250" height= "290" rel="external"></a>';
+                
+        }
+        
+     ?>
+    
+</body>
+</html>
